fc7897fdaf1318a6a6ff4769e1a0c6ea4111fdd2
twelve-xyx <52249099+twelve-xyx@users.noreply.github.com>
2019-07-23 14:57:17 +0800

    修改第一次

--- FastDFSDemo_SSM/src/main/java/com/itqf/controller/MyController.java
+++ FastDFSDemo_SSM/src/main/java/com/itqf/controller/MyController.java
@@ -13,13 +13,6 @@ import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @Description:
- * @Company: 千锋互联
- * @Author: 李丽婷
- * @Date: 2019/7/22
- * @Time: 上午11:01
- */
 @Controller
 public class MyController {
 
